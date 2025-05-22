5ed545a1c8e0da18b012c887b8f9acdb8201d960
Dzaki Ahnaf Z <61301953+dzaki236@users.noreply.github.com>
2025-05-22 21:51:38 +0700

    Update correct-statefull.dart

--- correct-statefull.dart
+++ correct-statefull.dart
@@ -1,1 +1,48 @@
+import 'package:flutter/material.dart';
 
+class StatefullW extends StatefulWidget {
+  const StatefullW({super.key});
+
+  @override
+  State<StatefullW> createState() => _StatefullWState();
+}
+
+class _StatefullWState extends State<StatefullW> {
+  final TextEditingController textController = TextEditingController();
+  String texts = '';
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'Name'),
+                  controller: textController,
+                  onChanged: (value) {
+                    setState(() {
+                      texts = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                Text(texts, style: TextStyle(fontSize: 22)),
+              ],
+            ),
+          ),
+        ),
+      ),
+      appBar: AppBar(title: Text('Test 2')),
+    );
+  }
+}
